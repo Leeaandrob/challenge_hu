aebbbdbcccd6a4cfd2daf7e8800556c4d4b7be14
Leandro Barbosa <leandrobar93@gmail.com>
2016-04-06 23:36:24 -0300

    register admin

--- search_hu/search_engine/admin.py
+++ search_hu/search_engine/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Establishment, AvailabilityEstablishment
+
+
+admin.site.register(Establishment)
+admin.site.register(AvailabilityEstablishment)
