2277f3d65199a932b976d38864f3c0e42f58f2cb
Leandro Barbosa <leandrobar93@gmail.com>
2016-04-07 23:15:23 -0300

    adding fields in form

--- search_hu/search_engine/forms.py
+++ search_hu/search_engine/forms.py
@@ -5,3 +5,5 @@ from django import forms
 class EstablishmentForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Search by Name'}))
+    checkin = forms.DateField()
+    checkout = forms.DateField()
