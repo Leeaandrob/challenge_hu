8b5a5a8437f03f46022ffa0983684613943da133
Leandro Barbosa <leandrobar93@gmail.com>
2016-04-07 22:51:33 -0300

    remove forms and add forms in search_engine

---
+++ search_hu/search_engine/forms.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from django import forms
+
+
+class EstablishmentForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Search by Name'}))
