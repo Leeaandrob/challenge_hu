4bf34308dc41477803d05dd586fcb115a252b0ac
Leandro Barbosa <leandrobar93@gmail.com>
2016-04-08 00:57:13 -0300

    update homesite for autocomplete and finish the queryset of search

--- search_hu/homesite/views.py
+++ search_hu/homesite/views.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 import re
+import json
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import ListView, TemplateView
 from django.db.models import Q
+
+from haystack.query import SearchQuerySet
 
 from search_engine.forms import EstablishmentForm
 from search_engine.models import AvailabilityEstablishment
@@ -17,8 +20,13 @@ class SearchFormView(object):
 
     def post(self, request, *args, **kwargs):
         context = self.get_context_data()
-        if context['form'].is_valid():
-            request.session['values_post'] = context['form'].cleaned_data
+        form = context['form']
+        if form.is_valid():
+            request.session['values_post'] = form.cleaned_data
+            form.cleaned_data['checkin'] = form.cleaned_data[
+                'checkin'].strftime('%Y-%m-%d')
+            form.cleaned_data['checkout'] = form.cleaned_data[
+                'checkout'].strftime('%Y-%m-%d')
             return HttpResponseRedirect('establishments/')
         return super(SearchFormView,
                      self).render_to_response(context)
@@ -41,9 +49,26 @@ class EstablishmentListView(ListView):
     def get_queryset(self):
         queryset = AvailabilityEstablishment.objects.all()
         values = self.request.session.get('values_post')
-        return queryset.filter(name__icontains=values.get('name'))
+        return queryset.filter(
+            available=True,
+            establishment__name=values.get('name').split('-')[1].strip(),
+            date__range=[str(values.get('checkin')),
+                         str(values.get('checkout'))])
 
     def get_querystring_url(self):
         querystring = self.request.GET.urlencode()
         _query = re.sub(r'&?page=\d+', '', querystring)
         return '?%s' % _query if _query else '?'
+
+
+def autocomplete(request):
+    term = request.GET.get('term')
+    sqs = SearchQuerySet().filter(
+        Q(name__icontains=term) | Q(city__icontains=term) |
+        Q(city__startswith=term) | Q(name__startswith=term))
+    sugestoes = [(dict(
+        name=resultado.name,
+        city=resultado.city,
+    )) for resultado in sqs]
+    data = json.dumps(sugestoes)
+    return HttpResponse(data, content_type='application/json')
