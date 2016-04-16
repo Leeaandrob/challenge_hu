4b3170bef425c334fcca3641608d1ea2d35987e7
Leandro Barbosa <leandrobar93@gmail.com>
2016-04-16 13:55:15 -0300

    creating other search index

--- search_hu/search_engine/search_indexes.py
+++ search_hu/search_engine/search_indexes.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from haystack import indexes
-from search_engine.models import Establishment
+from search_engine.models import (Establishment, AvailabilityEstablishment)
 
 
 class EstablishmentIndex(indexes.SearchIndex, indexes.Indexable):
@@ -14,4 +14,17 @@ class EstablishmentIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
+
+
+class AvailabilityEstablishmentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='establishment__name')
+    available = indexes.BooleanField(model_attr='available')
+    date = indexes.DateField(model_attr='date')
+
+    def get_model(self):
+        return AvailabilityEstablishment
+
+    def index_queryset(self, using=None):
         return self.get_model().objects.all()
